6217783ae760ef2a97cb46bd7b5add50132064fb
inn3rGames <hi@inn3r.com>
2023-10-11 22:43:50 +0300

    Add solution comments

--- client/src/queries/index.ts
+++ client/src/queries/index.ts
@@ -1,5 +1,13 @@
 import { gql } from '@apollo/client';
 
+/**
+ * We start fixing the GraphQL query by taking a look at our client.
+ * We notice that our TransactionsList component queries transactions using the "GetAllTransactions" query.
+ * Since we are getting a 400 status code from the server we start comparing the schemas from both the client and the server.
+ * We can clearly see that the "receipt" field is not present on the server schema and we also remove it from the client schema.
+ * Now our query is valid.
+ */
+
 export const GetAllTransactions = gql`
   query GetAllTransactions {
     getAllTransactions {
